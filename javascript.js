3395b02b8e52730b675fef38fc3f7223f58735dc
Rishav Walde <69449560+theri6v@users.noreply.github.com>
2023-10-06 01:26:24 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-// Flash the logo on hover
-const logo = document.getElementById('logo');
-logo.addEventListener('mouseover', () => {
-    logo.style.animation = 'flash 0.5s infinite';
-});
-
-logo.addEventListener('mouseout', () => {
-    logo.style.animation = 'none';
-});
